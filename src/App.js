5fdecf704c2888aec57928f261e2282eda1765f4
Rizaldi <rizaldiarif7@gmail.com>
2020-09-26 17:40:44 +0700

    Fix: responsive vh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -7,7 +7,40 @@ import Home from "./pages/Home";
 import Opportunity from "./pages/Opportunity";
 import Solution from "./pages/Solution";
 
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
+
 function App() {
+  // const [dimensions, setDimensions] = React.useState({
+  //   height: window.innerHeight,
+  //   width: window.innerWidth,
+  // });
+
+  useEffect(() => {
+    // Resize
+    const debouncedHandleResize = debounce(function handleResize() {
+      // setDimensions({
+      //   height: window.innerHeight,
+      //   width: window.innerWidth,
+      // });
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }, 1000);
+
+    window.addEventListener("resize", debouncedHandleResize);
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  });
+
   return (
     <BrowserRouter>
       <Header />
